f5df4caca8963bdf649d2ec89fc5f2c133f16420
Bidushi <sf.bidushi@gmail.com>
2021-01-27 11:35:51 +0600

    Database+ViewModel+Repo

---
+++ Restaurant/Repositories/PaymentTypeRepository.cs
@@ -0,0 +1,33 @@
+﻿using Restaurant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Restaurant.Repositories
+{
+    public class PaymentTypeRepository
+    {
+        private RestaurantEntities objRestaurantEntities;
+        
+        public PaymentTypeRepository()
+        {
+            objRestaurantEntities = new RestaurantEntities();
+        }
+        public IEnumerable<SelectListItem> GetAllPaymentType()
+        {
+            IEnumerable<SelectListItem> objselectListItems = new List<SelectListItem>();
+            objselectListItems = (from obj in objRestaurantEntities.PaymentTypes
+                                  select new SelectListItem()
+                                  { 
+                                      Text = obj.PaymentType1,
+                                      Value = obj.PaymentTypeId.ToString(),
+                                      Selected = true
+                                  }
+                                  ).ToList();
+            return objselectListItems;
+
+        }
+    }
+}
---
+++ Restaurant/ViewModel/SignupViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Restaurant.ViewModel
+{
+    public class SignupViewModel
+    {
+        public int Id { get; set; }
+        public string username { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        public string usertype { get; set; }
+    }
+}
